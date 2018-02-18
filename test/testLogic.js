3c609f4f8e3ba5c28e6bc189c25b298a3c9868a9
dragon <120267583@qq.com>
2018-02-18 18:11:53 +0800

    添加逻辑操作

---
+++ test/testLogic.js
@@ -0,0 +1,8 @@
+const program= require ("./lisp/logic.json")
+const {createEnv} = require("../lib/env")
+const eval = require("../lib/eval").eval
+const globalEnv = createEnv()
+const {initGlobalEnv}= require("../lib/init")
+initGlobalEnv(globalEnv)
+const result = eval(program,globalEnv)
+console.log(result)
